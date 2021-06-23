42c7632b126c5ec3359534f4405d39615abbf6f6
luojin <773559646@qq.com>
2021-06-23 18:31:20 +0800

    玩react  hooks

---
+++ 造轮子/react-hooks/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import Page from './views/index'
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('root')
+);
---
+++ 造轮子/react-hooks/src/views/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+
+export default function index() {
+    return (
+        <div>
+            <Heade />
+            <Button />
+        </div>
+    )
+}
+
+
+
+
+function Heade() {
+    return (
+        <div style={{ marginTop: "20px" }}>头部asd</div>
+    )
+}
+
+function Button() {
+
+
+}
+
+function debounce() {
+
+}
