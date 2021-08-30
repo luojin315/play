00e7a88a15c7f1d6789f599cf78185802c6d792b
wenrouxiansheng <773559646@qq.com>
2021-08-30 18:28:40 +0800

    更新字符串写入解决办法，memo使用方法

--- 造轮子/react-hooks/src/index.js
+++ 造轮子/react-hooks/src/index.js
@@ -1,16 +1,17 @@
 import ReactDOM from "react-dom";
 import "./index.css";
-import Page from "./refsTest";
-import { countState } from "./redux/store";
-import { Provider } from "react-redux";
+import Page from "./usehook";
+// import { countState } from "./redux/store";
+// import { Provider } from "react-redux";
 
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <Provider store={countState}>
-    <BrowserRouter>
-      <Page />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Provider store={countState}>
+//     <BrowserRouter>
+//       <Page />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+ReactDOM.render(<Page />, document.getElementById("root"));
--- 造轮子/react-hooks/src/usehook/index.js
+++ 造轮子/react-hooks/src/usehook/index.js
@@ -1,22 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  memo,
+  PureComponent,
+} from "react";
+
+const MemodFuncComponent = memo(Title);
 
 export default function Index() {
-    console.log("ASdsa")
-    return (
-        <div>
-            {useNum().num}
-        </div>
-    )
+  const [state, setState] = useState(0);
+  const [obj, setobj] = useState({ name: "zhangsan" });
+
+  //   const memoizedValue = useMemo(() => new Date().getFullYear(), [state]);
+  return (
+    <div>
+      usenum: {useNum().num}
+      <hr />
+      state: {state}
+      <MemodFuncComponent value={state} obj={obj} />
+      <button
+        onClick={() => {
+          setState(state + 1);
+        }}
+      >
+        click me!
+      </button>
+    </div>
+  );
 }
 
 function useNum() {
-    const [state, setState] = useState({ num: 0 });
-    useEffect(() => {
-        console.log("date effect");
-        let timer = setInterval(() => {
-            setState({ num: state.num + 1 });
-        }, 1000)
-        return () => clearInterval(timer)
-    }, [state])
-    return state;
+  const [state, setState] = useState({ num: 0 });
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setState({ num: state.num + 1 });
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [state]);
+  return state;
+}
+
+function Title({ obj, value }) {
+  console.log("asdasd");
+  return (
+    <h1>
+      {obj.name}title {value}
+    </h1>
+  );
 }
